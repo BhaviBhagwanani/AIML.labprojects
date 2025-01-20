067f5c8fb706661422f5a8814d13c734bd5c3023
BhaviBhagwanani <146053425+BhaviBhagwanani@users.noreply.github.com>
2025-01-20 10:49:34 +0530

    Create Datatype.py

---
+++ Datatype.py
@@ -0,0 +1,11 @@
+var1=int(input("enter a number"))
+var2=input("enter a string")
+var3=input("enter a string")
+var4=float(input("enter a number"))
+var5=input("enter True or False")
+sum=var1+var4
+concat=var2+var3
+if var5 == True:
+    print("it is true")
+print("the sum is ",sum)
+print("concatenation is ",concat)
